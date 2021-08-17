ba265e04eef9435c639790c7da5e774a9baef91d
Leandro Valovits <lvalovits@gmail.com>
2021-08-17 20:47:20 -0300

    commit desde cmd

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
     System.out.println(("test"));
-        //Aquí las instrucciones del método
+        //Aquí las instrucciones del método ok
 
 
     }
